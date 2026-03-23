dc3bf525f8f2717b211b6ccaba40e5960311755f
zeyuz35 <19887504+zeyuz35@users.noreply.github.com>
2026-03-23 22:59:01 +0000

    docs: fix missing S3 exports and add ... param in vis_miss

--- R/vis-miss.R
+++ R/vis-miss.R
@@ -39,6 +39,8 @@
 #'   column information will be presented when `facet` argument is used. Please
 #'   see the `naniar` package to provide missingness summaries over groups.
 #'
+#' @param ... Extra arguments passed to methods.
+#'
 #' @return `ggplot2` object displaying the position of missing values in the
 #'   dataframe, and the percentage of values missing and present.
 #'
@@ -253,35 +255,43 @@ vis_miss.data.frame <- function(
 
 
 # Time series methods: convert via tsbox to transposed data.frame, then dispatch.
+#' @export
 vis_miss.ts <- function(x, ...) {
   y <- ts_to_df(x)
   vis_miss.data.frame(y, ...)
 }
 
+#' @export
 vis_miss.mts <- function(x, ...) {
   vis_miss.data.frame(ts_to_df(x), ...)
 }
 
+#' @export
 vis_miss.zoo <- function(x, ...) {
   vis_miss.data.frame(ts_to_df(x), ...)
 }
 
+#' @export
 vis_miss.xts <- function(x, ...) {
   vis_miss.data.frame(ts_to_df(x), ...)
 }
 
+#' @export
 vis_miss.tbl_ts <- function(x, ...) {
   vis_miss.data.frame(ts_to_df(x), ...)
 }
 
+#' @export
 vis_miss.tbl_df <- function(x, ...) {
   vis_miss.data.frame(as.data.frame(x), ...)
 }
 
+#' @export
 vis_miss.tsibble <- function(x, ...) {
   vis_miss.data.frame(ts_to_df(x), ...)
 }
 
+#' @export
 vis_miss.default <- function(x, ...) {
   if (tsbox::ts_boxable(x)) {
     vis_miss.data.frame(ts_to_df(x), ...)
